16c5e6c9349ee9d9fa1746b0ddda90e70ab5ace5
Zuodion <zuodion@gmail.com>
2018-08-27 17:33:11 +0300

    Added production file with helmet and compression

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();//() - потому что функция
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));//info - тип в консоли
