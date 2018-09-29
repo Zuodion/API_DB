4b7a78c23f8170916cad3fcfb5b414668dd74453
Александр Иванов <>
2018-09-29 15:09:43 +0300

    TEST

--- routes/genres.js
+++ routes/genres.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
     });
 });
 
-router.post('/', async (req, res) => {
+router.post('/',tokenAuth, async (req, res) => {
 
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
