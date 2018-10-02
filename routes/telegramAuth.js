2628c78095657c545dab557cf7bd1244ef7fa715
Александр Иванов <>
2018-10-02 16:12:43 +0300

    Google

--- routes/telegramAuth.js
+++ routes/telegramAuth.js
@@ -4,9 +4,6 @@ const telegramCheckingAuthorization = require('telegram-checking-authorization')
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
-router.get('/', (req, res) => {
-  res.send('Hi')
-});
 
 router.post('/', (req, res) => {
   let token = "631428444:AAGB1Ot057LQ-tu-EgQ2BREgXw9m3_wfiuU";
