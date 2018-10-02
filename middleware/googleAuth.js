e2439f2cc2fccf6154fed1c363f20c114b732e1c
Александр Иванов <>
2018-10-02 17:40:21 +0300

    Google

--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const app = express();
 const https = require('https');
 
 
