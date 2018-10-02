28b159e579e18bd17e2baab76e7be6479680e9c5
Александр Иванов <>
2018-10-02 17:09:30 +0300

    Google

--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -5,8 +5,12 @@ const bodyParser = require("body-parser");
 
 router.post('/', async (req, res) => {
 const token = req.body.id_token;
-console.log(token)
-res.send(token)
+if (token){
+  app.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`, (res, res) => {
+    if res.status(200) console.log('GOOGLE OK');
+  })
+}
+
 });
 
 module.exports = router;
