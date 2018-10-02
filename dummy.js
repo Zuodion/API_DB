b75c724a73a610f392de010cf6e5228f39caabd7
Александр Иванов <>
2018-10-02 16:40:37 +0300

    Google

--- dummy.js
+++
@@ -1,6 +0,0 @@
-const telegramCheckingAuthorization = require('telegram-checking-authorization');
-let token = "631428444:AAGB1Ot057LQ-tu-EgQ2BREgXw9m3_wfiuU";
-let json = '{user":{"id":318302581,"first_name":"Alex","last_name":"Ivanov","username":"zuodion","photo_url":"https://t.me/i/userpic/320/zuodion.jpg","auth_date":1538477649,"hash":"f3e6e2765c9b45ad6478674075c676511bd19008def7dbdef4a47d497052a22c"}}';
-let data = JSON.parse(json);
-if(telegramCheckingAuthorization(data, token)) console.log('Data is from telegram!');
-if(!telegramCheckingAuthorization(data, token)) console.log('Data is NOT from telegram :(')
--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -7,19 +7,7 @@ const CLIENT_ID = '804233630975-8qdfrgfrh55dbgkbg5lk41ql8qmjukm6.apps.googleuser
 
 router.post('/', async (req, res) => {
 const token = req.body;
-req.send(token);
-const client = new OAuth2Client(CLIENT_ID);
-  verify().catch(console.error);
-  if(!error) console.log('OK GOOGLE')
-
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID
-      });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-  };
+console.log(token);
 });
 
 module.exports = router;
