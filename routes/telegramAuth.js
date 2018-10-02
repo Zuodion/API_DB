a851fefe13918a20d1bb774022a50c780409e962
Александр Иванов <>
2018-10-02 14:39:00 +0300

    Telegr

--- routes/telegramAuth.js
+++ routes/telegramAuth.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
-
+router.get('/', (req, res) => {
+  res.send('Hi')
+});
 
 router.post('/', jsonParser, (req, res) => {
   let token = "631428444:AAGB1Ot057LQ-tu-EgQ2BREgXw9m3_wfiuU";
