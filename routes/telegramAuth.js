c07be492f3915b3c3ac3c8dcfd830d4783e19959
Александр Иванов <>
2018-10-02 14:02:23 +0300

    Telegr

--- routes/telegramAuth.js
+++ routes/telegramAuth.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', (req, res) => {
   let token = "631428444:AAGB1Ot057LQ-tu-EgQ2BREgXw9m3_wfiuU";
-  let data = req.user;
+  let data = req.body.user;
   console.log(data)
   if(telegramCheckingAuthorization(data, token)) alert('Data is from telegram!');
   if(!telegramCheckingAuthorization(data, token)) alert('Data is NOT from telegram :(')
