cde2301e1e3b63b5828a342aa9678bdc9cf4200d
Александр Иванов <>
2018-10-02 14:04:59 +0300

    Telegr

--- routes/telegramAuth.js
+++ routes/telegramAuth.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const telegramCheckingAuthorization = require('telegram-checking-authorization');
 
-router.get('/', async (req, res) => {
-  res.send('lol')
-});
-
 router.post('/', (req, res) => {
   let token = "631428444:AAGB1Ot057LQ-tu-EgQ2BREgXw9m3_wfiuU";
-  let data = req.body.user;
+  let data = JSON.parse(req.data);
   console.log(data)
   if(telegramCheckingAuthorization(data, token)) alert('Data is from telegram!');
   if(!telegramCheckingAuthorization(data, token)) alert('Data is NOT from telegram :(')
