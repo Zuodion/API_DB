3e3ead3a9fc1a6ccf970e5e0d2c95b8a80a7ed4b
Александр Иванов <>
2018-10-02 16:42:34 +0300

    Google

--- middleware/googleAuth.js
+++ middleware/googleAuth.js
@@ -1,9 +1,7 @@
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = '804233630975-8qdfrgfrh55dbgkbg5lk41ql8qmjukm6.apps.googleusercontent.com';
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
 router.post('/', async (req, res) => {
 const token = req.body;
